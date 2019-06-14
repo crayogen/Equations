fbdc42823966525a65a869977876031d740bf845
Ibraheem Zaman <ibraheemzaman@gmail.com>
2019-06-15 00:47:56 +0500

    Remove unneeded validity check for empty text

--- app/src/main/java/com/example/equations/DragListener.kt
+++ app/src/main/java/com/example/equations/DragListener.kt
@@ -3,7 +3,6 @@ package com.example.equations
 import android.graphics.Color
 import android.view.DragEvent
 import android.view.View
-import android.widget.TextView
 
 class DragListener(
     private val isValid: (Item) -> Boolean,
@@ -14,15 +13,14 @@ class DragListener(
     override fun onDrag(targetView: View, event: DragEvent): Boolean {
         val dragData = event.localState as DragData
         val item = dragData.item
-        val isValidResult = (targetView as TextView).text.isEmpty() && isValid(item)
         when (event.action) {
             DragEvent.ACTION_DRAG_ENTERED ->
-                targetView.setBackgroundColor(if (isValidResult) Color.GREEN else Color.GRAY)
+                targetView.setBackgroundColor(if (isValid(item)) Color.GREEN else Color.GRAY)
             DragEvent.ACTION_DRAG_EXITED ->
                 targetView.setBackgroundColor(Color.BLACK)
             DragEvent.ACTION_DROP -> {
                 targetView.setBackgroundColor(Color.BLACK)
-                if (!isValidResult) return false
+                if (!isValid(item)) return false
                 dragData.clearTile()
                 populateTargetView(item)
                 onDropComplete()
