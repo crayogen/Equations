5ddc50d283e839371b66b8ed5530c1214fb0f8e4
Ibraheem Zaman <ibraheemzaman@gmail.com>
2019-06-16 06:21:42 +0500

    Convert colors to Kotlin and cast them to Int
    Ref: https://stackoverflow.com/a/44406886

---
+++ app/src/main/java/com/example/equations/Colors.kt
@@ -0,0 +1,9 @@
+package com.example.equations
+
+object Colors {
+    const val PINK = 0xFFFF8888.toInt()
+    const val GRAY = 0xFFAAAAAA.toInt()
+    const val BLUE = 0xFFA4C3E3.toInt()
+    const val WHITE = 0xFFFFFFFF.toInt()
+    const val DARK_GRAY = 0xFF444444.toInt()
+}
